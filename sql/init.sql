0fb4c6cc3c3e5114eb6c1651884761336a076e28
skovati <skovati@protonmail.com>
2022-07-15 13:46:59 -0700

    add postgres initialization and schema

---
+++ sql/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE account (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    email TEXT,
+    password TEXT
+);
+
+CREATE TABLE feed (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    url TEXT,
+    account_id INT,
+
+    CONSTRAINT feed_belongs_to_account
+        FOREIGN KEY (account_id)
+        REFERENCES account (id)
+        ON DELETE CASCADE
+);
